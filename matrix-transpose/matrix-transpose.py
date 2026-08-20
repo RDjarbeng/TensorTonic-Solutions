ea1edf2bccc5e1d449efd666866a60710909df9b
Richard Djarbeng <richarddjarbeng@gmail.com>
2026-08-20 14:38:36 +0200

    Solve matrix-transpose

--- matrix-transpose/matrix-transpose.py
+++ matrix-transpose/matrix-transpose.py
@@ -6,15 +6,10 @@ def matrix_transpose(A):
     """
     # Write code here
     m, n = len(A), len(A[0])
-    # print(m, n)
     At =np.zeros((n, m))
-    # print(At.shape)
     for i, row in enumerate(A):
-        # print(i)
-        # print(row)
         for j, column in enumerate(row):
             # print(j, column)
             At[j,i] = column
-    # print('At', At)
     return At
     pass
